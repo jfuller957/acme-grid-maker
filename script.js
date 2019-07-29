87e3358ec100f1f18cd9bd0984da94d358e8d550
James Fuller <jfuller957@gmail.com>
2019-07-29 10:43:35 -0700

    start action plan

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+Acme Grid Maker must...
+Build a form
+ - form takes in 2 numbers (rows by columns) to generate a grid of squares.
+  - form will only accept numbers, numbers must be between 1 and 100.
+  - form will have a button the user will click on to create the grid.
+
+The Grid
+  - When clicked the particular square of the grid will change color
+  - The changed color will remain until clicked again.
+  - below the grid when a cell is clicked the row index and the column index will be displayed.
+
+*/
